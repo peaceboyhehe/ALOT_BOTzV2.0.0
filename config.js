aa23cd72933fd12bbcde620e5724b356e1ff47a5
peaceboyhehe <104025430+peaceboyhehe@users.noreply.github.com>
2022-04-22 22:58:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /**
-   * Error Chat Gua !!!
-   wa.me/67572919291
+   * Error Chat !!!
+   wa.me/917902653386
 */
 
 const fs = require('fs')
@@ -17,25 +17,25 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285162634171','6285748894171']
-global.web = 'https://youtube.com/channel/UC2hMC3YSXSrDDhqOECXZkUg'
-global.bot = 'NahBotz-MD²'
+global.owner = ['917902653386','919947200835']
+global.web = 'Nothing Here !'
+global.bot = 'ALOT_BOTz'
 global.premium = ['6285748894171']
-global.packname = 'NakataArdnt'
-global.author = 'Nakata'
+global.packname = 'ALOT'
+global.author = 'Roar Insane'
 global.sessionName = 'nahh'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
     success: '✓ Success',
-    admin: 'Fitur Khusus Admin Group!',
-    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
-    owner: 'Fitur Khusus Owner Bot',
-    group: 'Fitur Digunakan Hanya Untuk Group!',
-    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
-    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    admin: 'Group Admin Special Feature!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'Owner Bots Special Feature',
+    group: 'Feature Used For Group Only!',
+    private: 'Feature Used Only For Private Chat!',
+    bot: 'Special Features for Users Bot Number',
     wait: 'Loading...',
-    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+    endLimit: 'Your Daily Limit Has Expired, Limit Will Reset Every 12 Hours',
 }
 global.limitawal = {
     premium: "Infinity",
